e0ddf034fa883fe9823fcd1fa37a68e4776e41fe
MateusSA <99056308+mateus-s-a@users.noreply.github.com>
2025-04-10 22:20:12 -0400

    Delete Eng. da Computação directory

--- Eng. da Computação/ordenacao/func.h
+++
@@ -1,2 +0,0 @@
-void menu(int *vetor, int tamanho);
-void impressaoVetor(int *vetor, int tamanho);
--- Eng. da Computação/ordenacao/ordenacao.h
+++
@@ -1,6 +0,0 @@
-void bubbleSort(int* vetor, int tamanho);
-
-int valorMinimo(int *vetor, int i, int tamanho);
-void ordenacaoPorSelecao(int *vetor, int tamanho);
-
-void ordenacaoPorInsercao(int *vetor, int tamanho);
