c35c7909543b6910b90e92911d923bb4443300b0
MateusSA <99056308+mateus-s-a@users.noreply.github.com>
2025-05-22 11:30:40 -0400

    Add files via upload

---
+++ POO/ex_21-05/src/br/com/compracerta/produtos/modelo/Produto.java
@@ -0,0 +1,47 @@
+package br.com.compracerta.produtos.modelo;
+
+public class Produto {
+    private int id = 1;
+    private String descricao;
+    private double preco;
+    private int qtd_estoque;
+
+    public Produto(int id, String descricao, double preco, int qtd_estoque) {
+        this.id = id;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.qtd_estoque = qtd_estoque;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQtdEstoque() {
+        return qtd_estoque;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQtd_estoque(int qtd_estoque) {
+        this.qtd_estoque = qtd_estoque;
+    }
+}
