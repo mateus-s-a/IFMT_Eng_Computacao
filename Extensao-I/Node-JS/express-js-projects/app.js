4f095e7e24e2f48c1e3a534a2e862fcc999ac4d4
Spacery <msa.mt314@gmail.com>
2025-08-26 23:45:06 -0400

    feat(express-js-projects): add API route in app.js to process user data and log username/email

--- Extensao-I/Node-JS/express-js-projects/app.js
+++ Extensao-I/Node-JS/express-js-projects/app.js
@@ -13,10 +13,12 @@ const logger = (req, res, next) => {                                            
 }
 
 // --- APPLY THE MIDDLEWARE ---
-app.use(logger);        // It tells Express.js to 'use()' 'logger' for every single request that comes in. MUST be replaced before the routes
+app.use(logger);                                // It tells Express.js to 'use()' 'logger' for every single request that comes in. MUST be replaced before the routes
 
 // --- SERVE STATIC FILLES (public) ---
-app.use(express.static('public'));                  // This Middleware tells Express.js to look for any requestered files in the 'public' folder
+app.use(express.static('public'));              // This Middleware tells Express.js to look for any requestered files in the 'public' folder
+
+app.use(express.json());                        // This Middleware parses incoming JSON and makes it avaible on 'req.body'
 
 
 // --- ROUTES ---
@@ -32,8 +34,25 @@ app.get('/status', (req, res) => {                                              
         uptime: `${(os.uptime() / 60).toFixed(2)} minutes`
     };
 
-    res.json(statusInfo);           // 'res.json()' automatically converts a JS object to JSON, and sets the correct 'content-type' header
+    res.json(statusInfo);                       // 'res.json()' automatically converts a JS object to JSON, and sets the correct 'content-type' header
 });
+
+app.post('/api/users', (req, res) => {                                          // 'API' route to create a new user
+    const newUser = req.body;                                                   // Thanks to 'express.json()' middleware, 'req.body' contains the parsed JSON
+    console.log("Received a new user data: ", newUser);
+
+    if (!newUser.name || !newUser.email) {                                      // Sending back only a confirmation
+        return res.status(400).json({ error: 'Name and Email are required.' }); // If data is missing: Bad Request 400
+    }
+
+    res.status(201).json({                                                      // 201 = Created
+        message: 'User created successfully',
+        user: newUser
+    });
+});
+
+
+
 
 app.listen(PORT, () => {                                                        // Starts the Server
     console.log(`Express.js server running on: http://localhost:${PORT}`);
