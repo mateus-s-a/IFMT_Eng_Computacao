1dc857ae5abd69285c0e6a958c745a21b80d4a20
Spacery <msa.mt314@gmail.com>
2025-08-29 21:56:43 -0400

    feat(auth): add validation for user registration and login routes

--- Extensao-I/Node-JS/express-js-projects/middlewares/protect.js
+++ Extensao-I/Node-JS/express-js-projects/middlewares/protect.js
@@ -6,25 +6,20 @@ const protect = asyncHandler(async (req, res, next) => {
     let token;
 
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            token = req.headers.authorization.split(' ')[1];                // 1. Extrair o 'token' do cabeçalho 'Bearer <token>'
-            
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);      // 2. Verificar se o 'token' é válido
+        token = req.headers.authorization.split(' ')[1];                // 1. Extrair o 'token' do cabeçalho 'Bearer <token>'
+        
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);      // 2. Verificar se o 'token' é válido
 
-            // 3. Anexar o usuário à requisição 'req' para uso posterior
-            // Busca-se o usuário no Data Base para garantir que ele ainda existe
-            const userResult = await pool.query('SELECT id, name, email FROM users WHERE id = $1', [decoded.id]);
-            if (userResult.rows.length === 0) {
-                throw new Error('User not found');
-            }
-            req.user = userResult.rows[0];
-            
-            next();                 // Passa para o próximo middleware/rota 'next()'
-        } catch (error) {
-            console.error(error);
-            res.status(401);
-            throw new Error('Not authorized, token failed');
+        // 3. Anexar o usuário à requisição 'req' para uso posterior
+        // Busca-se o usuário no Data Base para garantir que ele ainda existe
+        const userResult = await pool.query('SELECT id, name, email FROM users WHERE id = $1', [decoded.id]);
+        if (userResult.rows.length === 0) {
+            res.status(401);                    // Define o status específico para este caso = Unauthorized
+            throw new Error("User not found, token's user doesn't exist anymore");
         }
+        req.user = userResult.rows[0];
+        
+        next();                 // Passa para o próximo middleware/rota 'next()'
     }
 
     if (!token) {
