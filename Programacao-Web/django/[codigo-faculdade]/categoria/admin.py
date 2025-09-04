2fa9031059880d2104e8f573ab29f4b3258d5f61
Matthew <msa.mt314@gmail.com>
2025-09-04 14:36:55 -0400

    config(admin): configurando admin part 1

--- Programacao-Web/django/[codigo-faculdade]/categoria/admin.py
+++ Programacao-Web/django/[codigo-faculdade]/categoria/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from categoria.models import Categoria
 
 # Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):                 # Clase para permitir uma apresentação de atribuição automática de dados para o slug
+    prepopulated_fields = {
+        'slug': ('categoria_nome', )
+    }
+
+    list_display = ('categoria_nome', 'slug')
+admin_site.register(Categoria)
