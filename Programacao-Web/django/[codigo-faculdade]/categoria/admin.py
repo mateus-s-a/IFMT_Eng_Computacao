08f3a7e9754b96e465cf4809720d11e5ea28d6a4
Matthew <msa.mt314@gmail.com>
2025-09-04 14:55:45 -0400

    fix: Correct Categoria model and admin registration
    - Modified Categoria model to inherit from models.Model.
    - Registered Categoria model with its CategoriaAdmin options.
    - Added __str__ method to Categoria model for better representation.

--- Programacao-Web/django/[codigo-faculdade]/categoria/admin.py
+++ Programacao-Web/django/[codigo-faculdade]/categoria/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from categoria.models import Categoria
+from .models import Categoria
 
 # Register your models here.
-class CategoriaAdmin(admin.ModelAdmin):                 # Clase para permitir uma apresentação de atribuição automática de dados para o slug
+class CategoriaAdmin(admin.ModelAdmin):
     prepopulated_fields = {
         'slug': ('categoria_nome', )
     }
-
     list_display = ('categoria_nome', 'slug')
-admin_site.register(Categoria)
+
+admin.site.register(Categoria)
--- Programacao-Web/django/[codigo-faculdade]/categoria/models.py
+++ Programacao-Web/django/[codigo-faculdade]/categoria/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Categoria():
+class Categoria(models.Model):
     categoria_nome = models.CharField(max_length=100, unique=True)
     descricao = models.TextField(max_length=300, blank=True)
     categoria_imagens = models.ImageField(upload_to='fotos/categoria')
     slug = models.SlugField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.categoria_nome
