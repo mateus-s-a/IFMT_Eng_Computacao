1a41fcf34cb0184440a4b3111f3a80de1ac27693
Matthew <msa.mt314@gmail.com>
2025-09-16 12:54:50 -0400

    feat(produtos): update visualizarLoja view to handle category filtering

--- Programacao-Web/django/loja_online/produtos/urls.py
+++ Programacao-Web/django/loja_online/produtos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # criando um endereço URL para direcionar a página principal da loja
     path('', views.visualizarLoja, name='loja'),
+    path('<slug:categoria_slug>/', views.visualizarLoja, name='produtos_por_categoria'),
 ]
--- Programacao-Web/django/loja_online/produtos/views.py
+++ Programacao-Web/django/loja_online/produtos/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Produto
+from categoria.models import Categoria
 
 # Create your views here.
-def visualizarLoja(request):
-    produtos = (Produto).objects.all().filter(esta_disponivel=True)
-    produtos_quant = produtos.count()       # adiciona contagem de produtos ao 'context'
+def visualizarLoja(request, categoria_slug=None):
+    categorias = None
+    produtos = None
+
+    if categoria_slug != None:
+        categorias = get_object_or_404(Categoria, slug=categoria_slug)
+        produtos = Produto.objects.filter(categoria=categorias, esta_disponivel=True)
+        produtos_quant = produtos.count()
+    else:
+        produtos = (Produto).objects.all().filter(esta_disponivel=True)
+        produtos_quant = produtos.count()       # adiciona contagem de produtos ao 'context'
 
     context = {
         'produtos': produtos,
